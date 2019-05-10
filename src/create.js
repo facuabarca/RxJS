94a907597bffcf89977b3bb33b1be86b7ad7ef54
InnoIt <innoit@InnoIts-MBP.home>
2019-05-10 20:25:05 +0200

    rxjs-from

--- src/create.js
+++ src/create.js
@@ -4,6 +4,14 @@ import { from } from 'rxjs';
 export default () => {
     /** start coding */
 
+    const myArray = [1,2,3,4,5];
 
+    const myPromise = new Promise(resolve => setTimeout(() => {
+        resolve('Hello World');
+    }, 2000));
+
+    const observable = from(myPromise);
+
+    const subscription = observable.subscribe(e => displayLog(e));
     /** end coding */
 }
